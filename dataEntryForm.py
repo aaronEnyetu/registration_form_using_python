447c6c73be4fd383f73c3c3a4b2831aa521c582a
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-05 21:57:11 -0800

    Printing Entered Data Successfully

--- dataEntryForm.py
+++ dataEntryForm.py
@@ -1,6 +1,31 @@
 import tkinter
 from tkinter import ttk
 
+def enter_data():
+ 
+    #print("Data Entered Successfully")
+    #User info
+    firstname = first_name_entry.get()
+    lastname = last_name_entry.get()
+    title = title_combobox.get()
+    age = age_spinbox.get()
+    nationality = nationality_combobox.get()
+
+    # Course Info
+
+    registration_status = reg_status_var.get()
+    numcourses = numcourses_spinbox.get()
+    numsemesters = numcourses_spinbox.get()
+
+
+    print("First name: ", firstname, "Last name: ", lastname)
+    print("Title: ", title, "Age: ", age, "Nationality: ", nationality)
+    print("# Courses: ", numcourses, "# Semesters: ", numsemesters)
+    print("Registration status", registration_status)
+    print("--------------------------------------------")
+    
+
+
 window = tkinter.Tk()
 window.title("Data Entry Form")
 
@@ -51,7 +76,10 @@ courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=20)
 
 
 registered_label = tkinter.Label(courses_frame, text="Registration Status")
-registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered")
+
+reg_status_var = tkinter.StringVar()
+registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered",
+                                       variable=reg_status_var, onvalue="Registered", offvalue="Not Registered")
 registered_label.grid(row=0, column=0)
 registered_check.grid(row=1, column=0)
 
@@ -77,13 +105,16 @@ for widget in user_info_frame.winfo_children():
     terms_frame = tkinter.LabelFrame(frame, text="Terms $ Conditions")
     terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
 
-    terms_check = tkinter.Checkbutton(terms_frame, text="I accept the terms and conditions")
+    accept_var = tkinter.StringVar(value="Not Accepted")
+
+    terms_check = tkinter.Checkbutton(terms_frame, text="I accept the terms and conditions",
+                                      variable=accept_var, onvalue="Accepted", offvalue="Not Accepted")
     terms_check.grid(row=0, column=0)
 
 #Button
 
-button = tkinter.Button(frame, text="Enter data")
-button.grid(row=3, column=0)        
+button = tkinter.Button(frame, text="Enter data", command= enter_data)
+button.grid(row=3, column=0, sticky="news", padx=20, pady=10)        
 
 
 window.mainloop()
