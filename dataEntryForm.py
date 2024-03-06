15fcdad0621f5faf10b267a4b89aaeeafcea077b
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-05 20:45:14 -0800

    Created labels and entries for data form

---
+++ dataEntryForm.py
@@ -0,0 +1,31 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Data Entry Form")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+
+# Saving USer Info
+
+user_info_frame = tkinter.LabelFrame(frame, text="User Information")
+user_info_frame.grid(row=0, column=0, padx=20, pady=20)
+
+first_name_label = tkinter.Label(user_info_frame, text="First Name")
+first_name_label.grid(row=0, column=0)
+last_name_label = tkinter.Label(user_info_frame, text="Last Name")
+last_name_label.grid(row=0, column=1)
+
+
+first_name_entry = tkinter.Entry(user_info_frame)
+last_name_entry = tkinter.Entry(user_info_frame)
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
+
+title = tkinter.Label(user_info_frame, text="Title")
+
+
+
+
+window.mainloop()
