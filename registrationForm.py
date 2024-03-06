780dd605284bb2ff1b2fadc7b8791198ff358a44
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-05 20:12:31 -0800

    Added labels and grids

--- registrationForm.py
+++ registrationForm.py
@@ -5,6 +5,16 @@ root.geometry("500x300")
 
 Label(root, text="Python Registration Form", font="arial 15 bold").grid(row=0, column=3)
 
+name = Label(root, text="Name")
+address = Label(root, text="Address")
+state = Label(root, text="State")
+phone = Label(root, text="Phone")
+email = Label(root, text="Email")
 
+name.grid(row=1, column=2)
+address.grid(row=2, column=2)
+state.grid(row=3, column=2)
+phone.grid(row=4, column=2)
+email.grid(row=5, column=2)
 
 root.mainloop()
